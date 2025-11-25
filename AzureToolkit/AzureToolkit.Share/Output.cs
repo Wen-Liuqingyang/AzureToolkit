17e4df2a2580368269b868a65f9224efb95fd28a
文柳轻扬 <wen-liuqingyang@outlook.com>
2025-11-25 16:59:38 +0800

    feat: 创建Output类，以改进一些输出方法。
    这次封装了 WriteWithColor 和 WriteLineWithColor两个方法，以便带颜色输出

---
+++ AzureToolkit/AzureToolkit.Share/Output.cs
@@ -0,0 +1,29 @@
+﻿namespace AzureToolkit.Share
+{
+    public class Output
+    {
+        /// <summary>
+        /// 使用指定颜色输出信息，并在输出后重置颜色，使用Console.Write实现
+        /// </summary>
+        /// <param name="message">要输出的信息</param>
+        /// <param name="color">输出颜色</param>
+        public static void WriteWithColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.Write($"{message}");
+            Console.ResetColor(); // 每次使用后及时重置颜色
+        }
+
+        /// <summary>
+        /// 使用指定颜色输出信息，并在输出后重置颜色，使用Console.WriteLine实现
+        /// </summary>
+        /// <param name="message">要输出的信息</param>
+        /// <param name="color">输出颜色</param>
+        public static void WriteLineWithColor(string message, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine($"{message}");
+            Console.ResetColor(); // 每次使用后及时重置颜色
+        }
+    }
+}
